ed5c227268faf0f14242d733ce62b3957775e57b
HusainCode <alshaikhahmed@hotmail.com>
2025-12-16 23:01:41 -0600

    Bulding FlightService

--- flight-tracker/common/src/main/java/com/flighttracker/common/FlightService.java
+++ flight-tracker/common/src/main/java/com/flighttracker/common/FlightService.java
@@ -1,12 +1,26 @@
 package com.flighttracker.common;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.util.logging.Logger;
+
+import javax.print.DocFlavor.STRING;
+
+import org.json.JSONObject;
+
 
 // https://rapidapi.com/aedbx-aedbx/api/aerodatabox/playground/apiendpoint_97755564-247f-411f-bef2-ad26a453e389
 // 1 GET /flights/airports/icao/{airport}/departures
 // 2 GET /flights/airports/icao/{airport}/arrivals
 // 3 GET /flights/{flight-number}/status
 
-public class Flight {
+public class FlightService {
+
+    // private static final Logger LOGGER = Logger.getLogger(null)
+    private static final String API_KEY = System.getenv("API_KEY");
+    private static final String API_HOST = "aerodatabox.p.rapidapi.com";
+
     private String flightId;              // unique ID (usually icao24 or AeroDataBox id)
     private String flightNumber;          
     private String callsign;
@@ -33,17 +47,17 @@ public class Flight {
     private FlightStatus status;
     private long lastUpdated;
 
-    public Flight() {
-    }
-
-    public Flight(String flightId, String flightNumber, String callsign, String departureAirportIcao,
+    public FlightService(String flightId, String flightNumber, String callsign, String departureAirportIcao,
                   String destinationAirportIcao, String departureAirportName, String destinationAirportName,
                   String scheduledDepartureTime, String actualDepartureTime, String scheduledArrivalTime,
                   String estimatedArrivalTime, double latitude, double longitude, double altitude,
                   double speed, FlightStatus status, long lastUpdated) {
     }
 
-    public String getFlightId() {
+    public static String getFlightId(String flightNumber) throws Exception {
+        if (API_KEY == null || API_KEY.isBlank()) {
+            throw IllegalStateException("Missing required API_KEY environment variable"); // stopped here! 
+        }
     }
 
     public void setFlightId(String flightId) {
