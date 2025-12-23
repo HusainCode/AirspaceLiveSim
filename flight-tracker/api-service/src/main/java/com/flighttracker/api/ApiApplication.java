928c3fc0ef51d9d0c97da5b20dfa404a8c87aa05
HusainCode <alshaikhahmed@hotmail.com>
2025-12-22 22:47:46 -0600

    Started API

--- flight-tracker/api-service/src/main/java/com/flighttracker/api/ApiApplication.java
+++ flight-tracker/api-service/src/main/java/com/flighttracker/api/ApiApplication.java
@@ -1,4 +1,19 @@
 package com.flighttracker.api;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * API Service entry point.
+ * Provides REST endpoints and WebSocket support for flight tracking.
+ */
+@SpringBootApplication(scanBasePackages = {
+        "com.flighttracker.api",
+        "com.flighttracker.common"
+})
 public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
 }
