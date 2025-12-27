61700b70203a7a5f4b220db390cff051bd5497c3
HusainCode <alshaikhahmed@hotmail.com>
2025-12-26 22:24:32 -0600

    SC

--- flight-tracker/api-service/src/main/java/com/flighttracker/api/SearchController.java
+++ flight-tracker/api-service/src/main/java/com/flighttracker/api/SearchController.java
@@ -1,4 +1,18 @@
 package com.flighttracker.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/search")
 public class SearchController {
+
+    @GetMapping("/flights")
+    public List<Flight> searchFlights(@RequestParam String query) {
+        return List.of();
+    }
 }
