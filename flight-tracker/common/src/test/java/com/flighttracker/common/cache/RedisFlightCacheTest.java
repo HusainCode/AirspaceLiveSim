489d53ad377c4f538376c791528b4e0236c11e0a
HusainCode <alshaikhahmed@hotmail.com>
2026-01-12 23:27:52 -0600

    testing the water

--- flight-tracker/common/src/test/java/com/flighttracker/common/cache/RedisFlightCacheTest.java
+++ flight-tracker/common/src/test/java/com/flighttracker/common/cache/RedisFlightCacheTest.java
@@ -1,4 +1,12 @@
 package com.flighttracker.common.cache;
 
+import org.junit.jupiter.api.Test;
+
 public class RedisFlightCacheTest {
+
+    @Test
+    void testSomething() {
+        // TODO: finish this test
+
+    }
 }
